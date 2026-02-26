f4337f643e1c157128926e045d2160d351c8d47a
Gabriel <g_machado1@hotmail.com>
2026-02-26 15:30:59 -0300

    test: update 'resetDatabase' test util to avoid fk constraints errors

--- test/integration/test-utils.ts
+++ test/integration/test-utils.ts
@@ -59,14 +59,21 @@ export async function createTestApp(): Promise<INestApplication> {
 export async function resetDatabase(app: INestApplication): Promise<void> {
   const prisma = app.get(PrismaService);
 
-  await prisma.$transaction([
-    prisma.expenseSplit.deleteMany(),
-    prisma.expense.deleteMany(),
-    prisma.settlement.deleteMany(),
-    prisma.groupMember.deleteMany(),
-    prisma.group.deleteMany(),
-    prisma.user.deleteMany(),
-  ]);
+  const tablenames = await prisma.$queryRaw<
+    Array<{ tablename: string }>
+  >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+
+  const tables = tablenames
+    .map(({ tablename }) => tablename)
+    .filter((name) => name !== '_prisma_migrations')
+    .map((name) => `"public"."${name}"`)
+    .join(', ');
+
+  try {
+    await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${tables} CASCADE;`);
+  } catch (error) {
+    console.log({ error });
+  }
 }
 
 export async function deleteAllEmails() {
